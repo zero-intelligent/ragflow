fa8e2eb83325992ffb828ceca4160477d7f3ee35
lilonghui <2276110742@qq.com>
2024-10-29 09:19:00 +0800

    支持批量导入

--- graphrag/graph2neo4j.py
+++ graphrag/graph2neo4j.py
@@ -3,7 +3,7 @@ from graphrag.db.neo4j import driver
 import networkx as nx
 from loguru import logger as log
 
-def graph2neo4j(graph:nx.Graph,nodeLabel_attr:List[str] = ['entity_type']):
+def graph2neo4j(graph: nx.Graph, nodeLabel_attr: List[str] = ['entity_type']):
     """
     将当前的 python 里的 nx.Graph 里的数据同步到 neo4j ;
     确保节点的属性和关系的属性全部同步过去,节点的属性和关系的属性schema是未知的;
@@ -16,30 +16,50 @@ def graph2neo4j(graph:nx.Graph,nodeLabel_attr:List[str] = ['entity_type']):
         log.error(f"graph shouldn't be None")
         return
     
+    max_node_name_length = 30
+    
     with driver.session() as session:
-        # 创建或融合节点
+        # 批量创建或融合节点
+        node_queries = []
         for node, attrs in graph.nodes(data=True):
+            if len(node) > max_node_name_length:
+                log.error(f"node name '{node}' too long, abandon importing to neo4j")
+                continue
             labels = [f':`{attrs[attr]}`' for attr in nodeLabel_attr if attrs.get(attr)]
             label_str = "".join(labels) if labels else ''
-            node_properties = {**attrs, 'id': node}
-            cyber_query = f"""
-                MERGE (n{label_str} {{id: $id}})
-                ON CREATE SET n += {{{', '.join(f'{k}: ${k}' for k in attrs.keys())}}}
-                ON MATCH SET n += {{{', '.join(f'{k}: ${k}' for k in attrs.keys())}}}
-            """
-            log.debug(cyber_query)
-            session.run(cyber_query, **node_properties)
+            node_properties = ', '.join([f"{k}: '{v}'" for k, v in attrs.items()])
+            node_queries.append(f"""
+                MERGE (n{label_str} {{id: '{node}'}})
+                ON CREATE SET n += {{{node_properties}}}
+                ON MATCH SET n += {{{node_properties}}}
+            """)
 
-        # 创建或融合边
+        # 批量创建或融合边
+        edge_queries = []
         for source, target, attrs in graph.edges(data=True):
-            edge_properties = {**attrs, 'source': source, 'target': target}
-            cyber_query = f"""
-                MATCH (a:Node {{id: $source}}), (b:Node {{id: $target}})
-                MERGE (a)-[r:CONNECTED_TO]->(b)
-                ON CREATE SET r += {{{', '.join(f'{k}: ${k}' for k in attrs.keys())}}}
-                ON MATCH SET r += {{{', '.join(f'{k}: ${k}' for k in attrs.keys())}}}
-            """
-            log.debug(cyber_query)
-            session.run(cyber_query, **edge_properties)
+            if len(source) > max_node_name_length:
+                log.error(f"node name '{node}' too long, abandon importing to neo4j")
+                continue
+            if len(target) > max_node_name_length:
+                log.error(f"node name '{target}' too long, abandon importing to neo4j")
+                continue
             
-    log.info(f"{len(graph.nodes(data=True))} nodes, {len(graph.edges(data=True))} edges import to neo4j.")
+            edge_properties = ', '.join([f"{k}: '{v}'" for k, v in attrs.items()])
+            edge_queries.append(f"""
+                MATCH (a:Node {{id: '{source}'}}), (b:Node {{id: '{target}'}})
+                MERGE (a)-[r:CONNECTED_TO]->(b)
+                ON CREATE SET r += {{{edge_properties}}}
+                ON MATCH SET r += {{{edge_properties}}}
+            """)
+
+        # 执行节点的批量查询
+        if node_queries:
+            node_query_string = "CALL { " + " UNION ALL ".join(node_queries) + " }"
+            session.run(node_query_string)
+
+        # 执行边的批量查询
+        if edge_queries:
+            edge_query_string = "CALL { " + " UNION ALL ".join(edge_queries) + " }"
+            session.run(edge_query_string)
+
+    log.info(f"{len(graph.nodes(data=True))} nodes, {len(graph.edges(data=True))} edges imported to neo4j.")
