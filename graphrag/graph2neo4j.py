6e5a5e0434d1c0e3acf392dbc97d81269af19f8c
lilonghui <2276110742@qq.com>
2024-10-29 09:35:00 +0800

    添加日志

--- graphrag/graph2neo4j.py
+++ graphrag/graph2neo4j.py
@@ -22,11 +22,11 @@ def graph2neo4j(graph: nx.Graph, nodeLabel_attr: List[str] = ['entity_type']):
         # 批量创建或融合节点
         node_queries = []
         for node, attrs in graph.nodes(data=True):
-            if len(node) > max_node_name_length:
-                log.error(f"node name '{node}' too long, abandon importing to neo4j")
-                continue
             labels = [f':`{attrs[attr]}`' for attr in nodeLabel_attr if attrs.get(attr)]
             label_str = "".join(labels) if labels else ''
+            if len(node) > max_node_name_length:
+                log.warning(f"node {label_str} id: '{node}' too long, abandon importing to neo4j")
+                continue
             node_properties = ', '.join([f"{k}: '{v}'" for k, v in attrs.items()])
             node_queries.append(f"""
                 MERGE (n{label_str} {{id: '{node}'}})
@@ -38,10 +38,10 @@ def graph2neo4j(graph: nx.Graph, nodeLabel_attr: List[str] = ['entity_type']):
         edge_queries = []
         for source, target, attrs in graph.edges(data=True):
             if len(source) > max_node_name_length:
-                log.error(f"node name '{node}' too long, abandon importing to neo4j")
+                log.warning(f"node name '{node}' too long, connect edge in neo4j")
                 continue
             if len(target) > max_node_name_length:
-                log.error(f"node name '{target}' too long, abandon importing to neo4j")
+                log.warning(f"node name '{target}' too long, connect edge in neo4j")
                 continue
             
             edge_properties = ', '.join([f"{k}: '{v}'" for k, v in attrs.items()])
@@ -54,12 +54,12 @@ def graph2neo4j(graph: nx.Graph, nodeLabel_attr: List[str] = ['entity_type']):
 
         # 执行节点的批量查询
         if node_queries:
-            node_query_string = "CALL { " + " UNION ALL ".join(node_queries) + " }"
+            node_query_string = "CALL() { " + " UNION ALL ".join(node_queries) + " } RETURN 1"
             session.run(node_query_string)
 
         # 执行边的批量查询
         if edge_queries:
-            edge_query_string = "CALL { " + " UNION ALL ".join(edge_queries) + " }"
+            edge_query_string = "CALL() { " + " UNION ALL ".join(edge_queries) + " } RETURN 1"
             session.run(edge_query_string)
 
     log.info(f"{len(graph.nodes(data=True))} nodes, {len(graph.edges(data=True))} edges imported to neo4j.")
