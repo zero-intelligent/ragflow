c7dadfb8696e8e8cb382898e9cd40caffb1a891c
lilonghui <2276110742@qq.com>
2024-10-30 08:45:18 +0800

    batch run

--- graphrag/graph2neo4j.py
+++ graphrag/graph2neo4j.py
@@ -4,6 +4,15 @@ from graphrag.db.neo4j import driver
 import networkx as nx
 from loguru import logger as log
 
+def escape(input_string):
+    if not isinstance(input_string,str):
+        return input_string
+    # 使用 str.replace 方法转义特殊字符
+    escaped_string = input_string.replace('\\', '\\\\')  # 先转义反斜杠
+    escaped_string = escaped_string.replace("'", "\\'")  # 转义单引号
+    escaped_string = escaped_string.replace('"', '\\"')   # 转义双引号
+    return escaped_string
+
 def graph2neo4j(graph: nx.Graph, nodeLabel_attr: List[str] = ['entity_type']):
     """
     将当前的 python 里的 nx.Graph 里的数据同步到 neo4j ;
@@ -17,20 +26,20 @@ def graph2neo4j(graph: nx.Graph, nodeLabel_attr: List[str] = ['entity_type']):
         log.error(f"graph shouldn't be None")
         return
     
-    max_node_name_length = 30
+    MAX_NODE_NAME_LENGTH = 128
     start = time.time()
     with driver.session() as session:
         # 批量创建或融合节点
         node_queries = []
         for node, attrs in graph.nodes(data=True):
-            labels = [f':`{attrs[attr]}`' for attr in nodeLabel_attr if attrs.get(attr)]
+            labels = [f':`{escape(attrs[attr])}`' for attr in nodeLabel_attr if attrs.get(attr)]
             label_str = "".join(labels) if labels else ''
-            if len(node) > max_node_name_length:
+            if len(node) > MAX_NODE_NAME_LENGTH:
                 log.warning(f"node {label_str} id: '{node}' too long, abandon importing to neo4j")
                 continue
-            node_properties = ', '.join([f"{k}: '{v}'" for k, v in attrs.items()])
+            node_properties = ', '.join([f"{k}: {"'" + escape(v) + "'"  if isinstance(v, str) else v}" for k, v in attrs.items()])
             node_queries.append(f"""
-                MERGE (n{label_str} {{id: '{node}'}})
+                MERGE (n{label_str} {{id: '{escape(node)}'}})
                 ON CREATE SET n += {{{node_properties}}}
                 ON MATCH SET n += {{{node_properties}}}
             """)
@@ -38,31 +47,36 @@ def graph2neo4j(graph: nx.Graph, nodeLabel_attr: List[str] = ['entity_type']):
         # 批量创建或融合边
         edge_queries = []
         for source, target, attrs in graph.edges(data=True):
-            if len(source) > max_node_name_length:
-                log.warning(f"source node id:'{node}' too long to connect edge in neo4j")
+            if len(source) > MAX_NODE_NAME_LENGTH:
+                log.warning(f"source node id:'{source}' too long to connect edge in neo4j")
                 continue
-            if len(target) > max_node_name_length:
+            if len(target) > MAX_NODE_NAME_LENGTH:
                 log.warning(f"target node id:'{target}' too long to connect edge in neo4j")
                 continue
             
-            edge_properties = ', '.join([f"{k}: '{v}'" for k, v in attrs.items()])
+            edge_properties = ', '.join([f"{k}: '{escape(v)}'" for k, v in attrs.items()])
             edge_queries.append(f"""
-                MATCH (a:Node {{id: '{source}'}}), (b:Node {{id: '{target}'}})
+                MATCH (a:Node {{id: '{escape(source)}'}}), (b:Node {{id: '{escape(target)}'}})
                 MERGE (a)-[r:CONNECTED_TO]->(b)
                 ON CREATE SET r += {{{edge_properties}}}
                 ON MATCH SET r += {{{edge_properties}}}
             """)
 
-        # 执行节点的批量查询
+        BATCH_SIZE = 50
+        # 执行节点的批量
         if node_queries:
-            node_query_string = "CALL() { " + " UNION ALL ".join(node_queries) + " } RETURN 1"
             log.info(f"importing {len(node_queries)} nodes to neo4j")
-            session.run(node_query_string)
+            batchs = [node_queries[i:i + BATCH_SIZE] for i in range(0, len(node_queries), BATCH_SIZE)]
+            for batch in batchs:
+                node_query_string = "CALL() { " + " UNION ALL ".join(batch) + " } RETURN 1"
+                session.run(node_query_string)
 
-        # 执行边的批量查询
+        # 执行边的批量
         if edge_queries:
-            edge_query_string = "CALL() { " + " UNION ALL ".join(edge_queries) + " } RETURN 1"
             log.info(f"importing {len(edge_queries)} edges to neo4j")
-            session.run(edge_query_string)
+            batchs = [edge_queries[i:i + BATCH_SIZE] for i in range(0, len(edge_queries), BATCH_SIZE)]
+            for batch in batchs:
+                edge_query_string = "CALL() { " + " UNION ALL ".join(batch) + " } RETURN 1"
+                session.run(edge_query_string)
             
         log.info(f"{len(node_queries)} nodes, {len(edge_queries)} edges imported to neo4j, last:{time.time()-start:.2f}s")
