2bcba44816480c3348b960965e04f92f78198ab9
lilonghui <2276110742@qq.com>
2024-10-30 14:58:13 +0800

    将用户添加到同一个租户下，共享操作知识库

---
+++ conf/user2tenant.sql
@@ -0,0 +1,27 @@
+
+-- 将用户添加到同一个租户下，共享操作知识库
+
+-- lilonghui
+insert into user_tenant (id,create_time,create_date,update_time,update_date,user_id,tenant_id,role,invited_by,status)
+values(REPLACE(UUID(), '-', ''),1727842485858,'2024-10-02 04:14:45',1727842485858,'2024-10-02 04:14:45','a893285a87c311efb16f0242ac120006','7d19a176807611efb0f80242ac120006','normal','7d19a176807611efb0f80242ac120006',1);
+
+
+-- chenyuyun
+insert into user_tenant (id,create_time,create_date,update_time,update_date,user_id,tenant_id,role,invited_by,status)
+values(REPLACE(UUID(), '-', ''),1727842485858,'2024-10-02 04:14:45',1727842485858,'2024-10-02 04:14:45','5603c016856311efbaef0242ac120006','7d19a176807611efb0f80242ac120006','normal','7d19a176807611efb0f80242ac120006',1);
+
+
+-- wangli
+insert into user_tenant (id,create_time,create_date,update_time,update_date,user_id,tenant_id,role,invited_by,status)
+values(REPLACE(UUID(), '-', ''),1727842485858,'2024-10-02 04:14:45',1727842485858,'2024-10-02 04:14:45','79949d6a965d11efb03b0242ac120006','7d19a176807611efb0f80242ac120006','normal','7d19a176807611efb0f80242ac120006',1);
+
+-- jcg
+insert into user_tenant (id,create_time,create_date,update_time,update_date,user_id,tenant_id,role,invited_by,status)
+values(REPLACE(UUID(), '-', ''),1727842485858,'2024-10-02 04:14:45',1727842485858,'2024-10-02 04:14:45','973038ac8b7211efbf980242ac120003','7d19a176807611efb0f80242ac120006','normal','7d19a176807611efb0f80242ac120006',1);
+
+
+
+
+
+
+
