a22df2b046ef512a9287f65a1f50a0a038369ab6
lilonghui <2276110742@qq.com>
2024-11-04 11:00:45 +0800

    batch api 调用进度信息缓存到文件，确保每次不会重复调用api

--- rag/llm/batch_model.py
+++ rag/llm/batch_model.py
@@ -1,16 +1,20 @@
+from dataclasses import asdict, dataclass
 import hashlib
 import json
 import os
-from pathlib import Path
 import time
+import atexit
+from typing import Optional
+from pathlib import Path
 from loguru import logger as log
 from openai import OpenAI
 from rag.llm.chat_model import Base
 from rag.utils import assure_security, tries
-
+    
 class BatchModel:
     model:str
     client:OpenAI
+    
     def __init__(self,
                  model_instance: Base = None,
                  model: str = 'qwen-plus',
@@ -24,15 +28,17 @@ class BatchModel:
             self.model = model
             self.client = OpenAI(key,base_url)
             
+            
     @tries()
     def get_batch(self,bid):
         batch =  self.client.batches.retrieve(bid)
+        log.info(f"#### batch query ###### bid={bid},status:{batch.status}")
         if batch.status in ['failed','expired','cancelling','cancelled']:
             raise ValueError(batch)
         return batch
 
     @tries()
-    def file_upload(self,file:str='./res/text') -> str:
+    def file_upload(self,file:str) -> str:
         # test.txt 是一个示例文件
         file_object = self.client.files.create(file=Path(file), purpose="batch")
         log.debug(f"file created:{file_object.model_dump_json()}")
@@ -77,39 +83,49 @@ class BatchModel:
                     }
                 } for id, msg in id_messages.items()]
         
-        file = self.write_file(chat_input_lines)
-        output_file = file.with_suffix('.out.json')
-        if output_file.exists():
-            log.info(f"{output_file} exists, skip api_call")
-            with open(output_file, 'r') as file:
-                return json.load(file)
+        content = "\n".join([json.dumps(line, ensure_ascii=False) for line in chat_input_lines])
+        id = self.hash(content)
+        task = get_task(id)
+        
+        if not task.local_input_file:
+            task.local_input_file = str(self.write_file(file=id,content=content))
         
         
-        fid = self.file_upload(file)
-        log.info(f"##########  uploaded {file},fid={fid}")
+        if not task.server_input_file_id:
+            task.server_input_file_id = self.file_upload(task.local_input_file)
+        
+        if not task.batch_id:
+            task.batch_id = self.batch_create(task.server_input_file_id)
+        
+        # 一般情况，任务在等待过程中中断，因此期望在此临时保存下任务。
+        save_task(task)
 
-        bid = self.batch_create(fid)
-        log.info(f"########## {file},bid={bid}")
-
-        chat_results = []
-        while True:
+        while task.batch_status != 'completed':
             time.sleep(int(os.environ.get('BATCH_QUERY_INTERVAL',60)))
-            batch = self.get_batch(bid)
-            log.info(f"#### batch query ###### bid={bid},status:{batch.status}")
-            if batch.status == 'completed':
-                chat_results = self.get_results(batch.output_file_id)
-                break
-        
+            batch = self.get_batch(task.batch_id)
+            task.batch_status = batch.status
+            
+        if not task.server_output_file_id:
+            chat_results = self.get_results(batch.output_file_id)
+            task.server_output_file_id = batch.output_file_id
+                
         # 校验是否有丢失的数据没有返回来
         input_ids = [line['custom_id'] for line in chat_input_lines]
         not_back_ids = set(input_ids) - set(chat_results.keys())
         if not_back_ids:
-            log.error(f"{file} 中的 custom_id in {not_back_ids} not back, may be secure blocked by server.")
+            log.error(f"{task.local_input_file} 中的 custom_id in {not_back_ids} not back, may be secure blocked by server.")
         
-        
-        log.info(f"##########  chat_results dumps to {output_file}")
-        with open(output_file, 'w') as file:
-            json.dump(chat_results, file, ensure_ascii=False,indent=4)
+        if not task.local_output_file \
+        or not Path(task.local_output_file).exists():
+            filepath = Path(task.local_input_file).with_suffix('.out.json')
+            with open(filepath, 'w') as file:
+                json.dump(chat_results, file, ensure_ascii=False,indent=4)
+            log.info(f"##########  chat_results dumps to {file}")
+            task.local_output_file = str(file)
+        else:
+            with open(task.local_output_file, 'r') as file:
+                return json.load(file)
+            log.info(f"{task.local_output_file} exists, skip api_call")
     
         return chat_results
           
@@ -123,14 +139,13 @@ class BatchModel:
         return hash_value
             
             
-    def write_file(self,input_items, inputs_dir:str="./inputs"):
+    def write_file(self,file, content, inputs_dir:str="./inputs"):
         """
          input_items 的 hash key 将作为缓存的 key ,确保不需要反复调用 LLM
         """
         try:
-            content = "\n".join([json.dumps(line, ensure_ascii=False) for line in input_items])
-            f_name = self.hash(content) + ".jsonl"
-            file = Path(inputs_dir) / f_name
+            
+            file = Path(inputs_dir) / file
             Path(inputs_dir).mkdir(exist_ok=True)
             if file.exists():
                 log.info(f"{file} exists, skip writing")
@@ -139,9 +154,60 @@ class BatchModel:
             with open(file, 'w') as f:
                 log.info(f"########## file={file}")
                 f.write(content)
-            return file
+            return str(file)
         except Exception as e:
             log.error("写入文件时发生错误" + str(e))
             exit(1)
             
 
+
+@dataclass
+class BatchTaskInfo:
+    """
+    存储进度信息，避免中断后重复从头执行导致的成本增加
+    """
+    id: str
+    local_input_file: Optional[str] = None
+    server_input_file_id: Optional[str] = None
+    batch_id: Optional[str] = None
+    batch_status: Optional[str] = None
+    server_output_file_id: Optional[str] = None
+    local_output_file: Optional[str] = None
+        
+
+def get_task(id:str):
+    if id not in batch_tasks:
+        batch_tasks[id] = BatchTaskInfo(id=id)
+    return batch_tasks[id]
+
+def load_tasks():
+    batch_tasks = {}
+    if Path(tasks_file).exists():
+        with open(tasks_file, 'r') as file:
+            for line in file:
+                json_dict = json.loads(line)
+                task_info = BatchTaskInfo(**json_dict)
+                batch_tasks[task_info.id] = task_info
+            log.info(f"{len(batch_tasks)} tasks load from {tasks_file}")
+    return batch_tasks
+
+def save_task(task:BatchTaskInfo):
+    batch_tasks[task.id] = task
+    with open(tasks_file, 'a', encoding='utf-8') as file:
+        file.write(json.dumps(asdict(task), indent=4) + "\n")
+    log.info(f"{asdict(task)} saved to {tasks_file}")
+
+def save_all_task():
+    with open(tasks_file, 'w', encoding='utf-8') as file:
+        for task in batch_tasks:
+            file.write(json.dumps(asdict(task), indent=4) + "\n")
+    log.info(f"{len(batch_tasks)} tasks saved to {tasks_file}")
+    
+# 存储进度信息
+tasks_file:str = ".tasks.json"
+
+# 启动时，装载上次失败的任务
+batch_tasks = load_tasks()
+
+# 退出时，确保所有任务进度保存
+atexit.register(save_all_task)
--- test/test_batch_api.py
+++ test/test_batch_api.py
@@ -6,7 +6,7 @@ def test_qwen_batch_api(tenant_id='7d19a176807611efb0f80242ac120006',
                         kb_id='fb7c4312973b11ef88ed0242ac120006',
                         parser_config = {'entity_types': ['pet-species (宠物种类)', 'breed (品种)', ' age (年龄)', ' gender (性别) ', 'weight (体重)', ' temperature (体温)', ' disease (疾病)', ' symptom (症状) ', 'medication (药物)', ' treatment-method (治疗方法)', ' diagnostic-test (诊断测试) ', 'sign (体征)', 'organ-or-system (器官或系统) ', 'vaccine (疫苗)', ' animal-behavior (动物行为) ', 'allergen (过敏源) ', 'prognosis (预后)', ' environmental-factors (环境因素)', ' nutrition (营养) ', 'food (食物)', ' water-intake (饮水情况) ', 'lifestyle (生活习惯) ', 'allergic-reaction (过敏反应)', ' living-environment (居住环境)', ' parasite (寄生虫)', ' restraint-method (保定法) ', 'examination-method (检查方法)', ' epidemiology (流行病学) ', 'lesion (病变)', 'prevention(预防方法)'], 
                                          'chunk_token_num': 8192, 'delimiter': '\\\\n!?;。；！？'},
-                        filename = '/home/admin/python_projects/MedicalGPT/data/pretrain/pet_books/宠医临床书籍/【07】（国家重大出版工程项目）《动物营养学（第六版）》.pdf.txt'):
+                        filename = '/home/admin/python_projects/MedicalGPT/data/pretrain/pet_books/宠医临床书籍/【44】猫病学.pdf.txt'):
         
     with open(filename, 'rb') as file:
         binary = file.read()
